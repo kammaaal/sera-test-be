c05a255c14b4c25e61762f85b9943bc8dac17354
kemalandraza <113872990+kemalandraza@users.noreply.github.com>
2025-01-20 07:27:21 +0700

    init secondary branch

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,24 +1,55 @@
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL_USERNAME,
-    pass: process.env.EMAIL_PASSWORD,
-  },
+// const transporter = nodemailer.createTransport({
+//     host: 'smtp.gmail.com',
+//     port: 587,
+//     secure: false,
+//     service: 'gmail',
+//     auth: {
+//         user: process.env.EMAIL_USERNAME,
+//         pass: process.env.EMAIL_PASSWORD,
+//     },
+//     debug: true,
+//     logger: true
+// });
+
+// // console.log("data email: ", transporter.auth.user)
+// const sendEmail = async ({ to, subject, text }) => {
+//   try {
+//     const info = await transporter.sendMail({
+//       from: process.env.EMAIL_USERNAME,
+//       to,
+//       subject,
+//       text,
+//     });
+//     console.log('Email sent:', info.messageId);
+//   } catch (error) {
+//     console.error('Failed to send email:', error.message);
+//   }
+// };
+
+const mailgun = require('mailgun-js');
+
+// Konfigurasi Mailgun menggunakan environment variables
+const mg = mailgun({
+  apiKey: process.env.MAILGUN_API_KEY,
+  domain: process.env.MAILGUN_DOMAIN,
 });
 
-const sendEmail = async ({ to, subject, text }) => {
+const sendEmail = async (to, subject, text) => {
+  const data = {
+    from: process.env.MAILGUN_FROM_EMAIL,
+    to,
+    subject,
+    text,
+  };
+
   try {
-    const info = await transporter.sendMail({
-      from: process.env.EMAIL_USERNAME,
-      to,
-      subject,
-      text,
-    });
-    console.log('Email sent:', info.messageId);
+    const result = await mg.messages().send(data);
+    return result;
   } catch (error) {
     console.error('Failed to send email:', error.message);
+    throw new Error('Email sending failed');
   }
 };
 
